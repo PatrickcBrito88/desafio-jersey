3ab50b48319042a27eae6b1a149feae2430cb697
patrick <patrickcbrito88@gmail.com>
2024-06-26 09:16:04 -0300

    feat: criação de constantes com scripts de banco de dados

--- src/main/java/org/brito/constantes/scripts.java
+++ src/main/java/org/brito/constantes/scripts.java
@@ -2,12 +2,12 @@ package org.brito.constantes;
 
 public class scripts {
 
-    public static String CRIA_DATABASE = "CREATE DATABASE porto";
-    public static String CRIA_TABELA_USUARIOS = "CREATE TABLE usuarios (" +
+    public static String CRIAR_DATABASE = "CREATE DATABASE porto";
+    public static String CRIAR_TABELA_USUARIOS = "CREATE TABLE usuarios (" +
             "id SERIAL PRIMARY KEY," +
             "login VARCHAR(255) UNIQUE NOT NULL," +
             "password VARCHAR(255) NOT NULL," +
             "role VARCHAR(50) NOT NULL)";
-    public static String INSERE_USUARIO = "INSERT INTO usuarios (login, password, role) VALUES (?, ?, ?)";
-    public static String VALIDA_USUARIO_SENHA = "SELECT * FROM usuarios WHERE login = ?";
+    public static String INSERIR_USUARIO = "INSERT INTO usuarios (login, password, role) VALUES (?, ?, ?)";
+    public static String BUSCAR_PASSWORD_PELO_LOGIN = "SELECT password FROM usuarios WHERE login = ?";
 }
