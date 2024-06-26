45b52ae200eec7d44886fdde9578bf4f455150b6
patrick <patrickcbrito88@gmail.com>
2024-06-26 09:14:13 -0300

    feat: criação de anotation e resource para verificacao de endpoint nao autenticado

--- src/main/java/org/brito/infrastructure/api/AuthenticationResource.java
+++ src/main/java/org/brito/infrastructure/api/AuthenticationResource.java
@@ -2,11 +2,12 @@ package org.brito.infrastructure.api;
 
 
 import jakarta.ws.rs.*;
-import org.brito.authentication.AuthToken;
-import org.brito.authentication.UsuarioCredenciais;
+import org.brito.annotations.NaoAutenticado;
+import org.brito.security.AuthTokenDTO;
+import org.brito.security.UsuarioCredenciaisDTO;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import org.brito.services.AuthenticationService;
+import org.brito.security.AuthenticationService;
 import org.brito.utils.MessageUtils;
 
 import java.util.Objects;
@@ -18,22 +19,27 @@ public class AuthenticationResource {
     @Path("/login")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response login(UsuarioCredenciais credentials) {
+    @NaoAutenticado
+    public Response login(UsuarioCredenciaisDTO credentials) {
         try {
-            String token = AuthenticationService.authenticate(credentials.getLogin(), credentials.getPassword());
+            String token = AuthenticationService.buscarToken(credentials.getLogin(), credentials.getPassword());
             if (Objects.isNull(token)) {
-                return Response.status(Response.Status.UNAUTHORIZED).entity(MessageUtils.buscaValidacao("credenciais.invalidas")).build();
+                return Response.status(
+                        Response.Status.UNAUTHORIZED).entity(
+                                MessageUtils.buscaValidacao("credenciais.invalidas")).build();
             }
-            return Response.ok(new AuthToken(token)).build();
+            return Response.ok(new AuthTokenDTO(token)).build();
         } catch (Exception e) {
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Server error").build();
+            return Response.status(
+                    Response.Status.INTERNAL_SERVER_ERROR).entity(
+                            MessageUtils.buscaValidacao("error.servidor")).build();
         }
     }
 
     @GET
-    @Path("/hello")
-    public Response hello() {
-        return Response.ok("Hello World!").build();
+    @Path("/teste")
+    public Response teste() {
+        return Response.ok("Teste").build();
     }
 
 }
